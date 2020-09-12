1f0cec7238716da36881e5effd87df401aaf389c
anzor_urdia <aurdia@vaba.co>
2020-09-12 19:04:33 +0400

    redis configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 #conf = Rails.application.config_for(:redis)['redis_auth_conf'].symbolize_keys
 
-$redis = Redis.new({url: "redis://#{ENV['REDIS_URL']}:#{ENV['REDIS_PORT']}/#{2}"})
+$redis = Redis.new({url: "redis://127.0.0.1:6379/2"})
 #$redis = Redis.new({url: REDIS_URL})
+//127.0.0.1:6379/2
