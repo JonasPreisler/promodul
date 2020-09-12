a46546ee20f6fc3fa850d531cd60a713a37e425a
anzor_urdia <aurdia@vaba.co>
2020-09-12 21:51:39 +0400

    twilio configuration

--- app/services/integrations/twilio_request_service.rb
+++ app/services/integrations/twilio_request_service.rb
@@ -7,25 +7,21 @@ module Integrations
       @country_code = country_code
       @phone_number = phone_number
       @code = code
-      #binding.pry
-      # Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]  )
       @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
     end
 
     def start_verification
-      #@client = Twilio::REST::Client.new(Figaro.env.TWILIO_ACCOUNT_SID, Figaro.env.TWILIO_AUTH_TOKEN  )
       begin
-         @client.verify.services(Figaro.env.TWILIO_SERVICE_SID).verifications.create(locale: 'nb', to: "+#{@country_code}#{@phone_number}", channel: 'sms')
+         @client.verify.services(ENV["TWILIO_SERVICE_SID"]).verifications.create(locale: 'nb', to: "+#{@country_code}#{@phone_number}", channel: 'sms')
       rescue Twilio::REST::RestError => e
         Rails.logger.error "SMS Sending Error - #{e} - Confirmation Code, number: #{@phone_number}"
       end
     end
 
     def verify_code
-      #@client = Twilio::REST::Client.new(Figaro.env.TWILIO_ACCOUNT_SID, Figaro.env.TWILIO_AUTH_TOKEN  )
       begin
         verification_check = @client.verify
-                                 .services(Figaro.env.TWILIO_SERVICE_SID)
+                                 .services(ENV["TWILIO_SERVICE_SID"])
                                  .verification_checks
                                  .create(to: "+#{+@country_code}#{@phone_number}", code: @code)
         verification_check.status
