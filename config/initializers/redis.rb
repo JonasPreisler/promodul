94223e41e6b40018928bc560e0922766ee37f973
anzor_urdia <aurdia@vaba.co>
2020-09-12 18:48:12 +0400

    added listener

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
-conf = Rails.application.config_for(:redis)['redis_auth_conf'].symbolize_keys
-
-$redis = Redis.new({url: "redis://#{conf[:host]}:#{conf[:port]}/#{conf[:db]}"})
+#conf = Rails.application.config_for(:redis)['redis_auth_conf'].symbolize_keys
+#
+#$redis = Redis.new({url: "redis://#{conf[:host]}:#{conf[:port]}/#{conf[:db]}"})
+$redis = Redis.new({url: REDIS_URL})
