c0ab86e95fe7d0dbd144fa77f1ea06fd5aee6a96
anzor_urdia <aurdia@vaba.co>
2020-10-11 17:15:01 +0400

    added changes for front

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
-  has_many :sub_companies, class_name: 'Company', foreign_key: 'parent_id'
+  has_many :sub_companies, class_name: 'Company', foreign_key: 'parent_id', dependent: :destroy
   belongs_to :country
   belongs_to :city
   has_many :departments, dependent: :destroy
