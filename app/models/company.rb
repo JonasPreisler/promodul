a92cac2a1b14328502b01a2ef190bcced867e787
anzor_urdia <aurdia@vaba.co>
2020-10-12 16:16:33 +0400

    added changes into departments

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   has_one_attached :logo
-  has_one :company_logo, dependent: :destroy
+  has_one :company_logo
 
   has_many :sub_companies, class_name: 'Company', foreign_key: 'parent_id', dependent: :destroy
   belongs_to :country
