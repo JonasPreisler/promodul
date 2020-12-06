ccb2ffbdee853f91671f80a83576de16d1031654
anzor_urdia <aurdia@vaba.co>
2020-12-06 23:44:04 +0400

    added yoliferi

--- app/services/orders/orders_services.rb
+++ app/services/orders/orders_services.rb
@@ -78,7 +78,7 @@ module Orders
 
     def open_order_list
       @orders = Order
-                    .select("id, TO_CHAR(start_time, 'dd/MM/yyyy hh:mm:ss') as start, deadline as end")
+                    .select("id, TO_CHAR(start_time, 'dd-MM-yyyy hh:mm:ss') as start, deadline as end")
                     .where(order_status_id: 1)
     end
 
