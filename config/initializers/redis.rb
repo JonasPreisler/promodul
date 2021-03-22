71828da8f355aaae038d2f219a9d3f0739c6d61c
anzor_urdia <aurdia@vaba.co>
2021-03-23 00:32:10 +0400

    redis chnages

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 #conf = Rails.application.config_for(:redis)['redis_auth_conf'].symbolize_keys
-
-$redis = Redis.new({url: "redis://127.0.0.1:6379/2"})
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379/2" )
+$redis = Redis.new({url: uri})
 #$redis_auth = Redis.new({url: "redis://#{conf[:host]}:#{conf[:port]}/#{conf[:db]}"})
 #$redis_helper = Redis.new({url: "redis://#{conf_stat[:host]}:#{conf_stat[:port]}/#{conf_stat[:db]}"})
 #$redis_rate_limit = Redis.new({url: "redis://#{conf_rate[:host]}:#{conf_rate[:port]}/#{conf_rate[:db]}"})
