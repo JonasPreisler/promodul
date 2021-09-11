8acb0565ef28455b6a36c62ab5c226ead44c291e
anzor_urdia <aurdia@vaba.co>
2021-09-11 18:39:21 +0400

    fix uploader

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,15 +1,15 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['S3_ACCESS_ID'],
-      aws_secret_access_key: ENV['S3_ACCESS_KEY'],
-      path_style:             true,
-      region:                'eu-central-1'
-  }
-
-  config.fog_directory  = ENV['S3_BUCKET']
-  config.fog_public     = false
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-  config.storage = :fog
-end
+#CarrierWave.configure do |config|
+#  config.fog_provider = 'fog/aws'
+#  config.fog_credentials = {
+#      provider:              'AWS',
+#      aws_access_key_id:     ENV['S3_ACCESS_ID'],
+#      aws_secret_access_key: ENV['S3_ACCESS_KEY'],
+#      path_style:             true,
+#      region:                'eu-central-1'
+#  }
+#
+#  config.fog_directory  = ENV['S3_BUCKET']
+#  config.fog_public     = false
+#  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+#  config.storage = :fog
+#end
